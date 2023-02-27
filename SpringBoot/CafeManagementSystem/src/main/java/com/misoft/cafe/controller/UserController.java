0d2d61f3a13ebf7ca82a1e2f2c56ea345317be1b
Mazharul Islam Rakib <mirakib33@gmail.com>
2023-02-27 19:33:49 +0600

    Revert "Revert "get api done""
    This reverts commit 4b2bab9b374a2f141e6313ad6685ef3f04d2ef2f.

--- SpringBoot/CafeManagementSystem/src/main/java/com/misoft/cafe/controller/UserController.java
+++ SpringBoot/CafeManagementSystem/src/main/java/com/misoft/cafe/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.misoft.cafe.controller;
 
+import com.misoft.cafe.wrapper.UserWrapper;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 import java.util.Map;
 
 @RequestMapping(path = "/user")
@@ -15,5 +18,8 @@ public interface UserController {
 
     @PostMapping(path = "/login")
     public ResponseEntity<String> login(@RequestBody Map<String, String> requestMap);
+
+    @GetMapping(path = "/get")
+    public ResponseEntity<List<UserWrapper>> getAllUser();
 
 }
--- SpringBoot/CafeManagementSystem/src/main/java/com/misoft/cafe/controllerImpl/UserControllerImpl.java
+++ SpringBoot/CafeManagementSystem/src/main/java/com/misoft/cafe/controllerImpl/UserControllerImpl.java
@@ -4,11 +4,14 @@ import com.misoft.cafe.constants.CafeConstants;
 import com.misoft.cafe.controller.UserController;
 import com.misoft.cafe.service.UserService;
 import com.misoft.cafe.utils.CafeUtils;
+import com.misoft.cafe.wrapper.UserWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -35,6 +38,16 @@ public class UserControllerImpl implements UserController {
             ex.printStackTrace();
         }
         return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG , HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<List<UserWrapper>> getAllUser() {
+        try {
+            return userService.getAllUser();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<List<UserWrapper>>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
