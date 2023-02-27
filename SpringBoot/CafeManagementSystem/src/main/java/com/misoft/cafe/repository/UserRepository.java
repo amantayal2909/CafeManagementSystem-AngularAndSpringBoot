cc39c01a21f6a4c87881dffb178e531225b8bcee
Mazharul Islam Rakib <mirakib33@gmail.com>
2023-02-27 19:33:58 +0600

    Revert "Revert "Update user complete""
    This reverts commit 1db81ad98ef76eb901e1b2bd26bd962de9ca73d8.

--- SpringBoot/CafeManagementSystem/src/main/java/com/misoft/cafe/repository/UserRepository.java
+++ SpringBoot/CafeManagementSystem/src/main/java/com/misoft/cafe/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.misoft.cafe.repository;
 import com.misoft.cafe.entity.User;
 import com.misoft.cafe.wrapper.UserWrapper;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -15,4 +17,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     List<UserWrapper> getAllUser();
 
+    @Transactional
+    @Modifying
+    Integer updateStatus(@Param("status") String status, @Param("id") Integer id);
 }
