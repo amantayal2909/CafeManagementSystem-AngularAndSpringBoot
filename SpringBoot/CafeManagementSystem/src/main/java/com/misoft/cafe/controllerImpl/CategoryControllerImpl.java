e68d903771a68d99eccfd4917cb6c5997b53a1e2
Mazharul Islam Rakib <mirakib33@gmail.com>
2023-03-02 21:27:14 +0600

    get and update category done

--- SpringBoot/CafeManagementSystem/src/main/java/com/misoft/cafe/controllerImpl/CategoryControllerImpl.java
+++ SpringBoot/CafeManagementSystem/src/main/java/com/misoft/cafe/controllerImpl/CategoryControllerImpl.java
@@ -2,6 +2,7 @@ package com.misoft.cafe.controllerImpl;
 
 import com.misoft.cafe.constants.CafeConstants;
 import com.misoft.cafe.controller.CategoryController;
+import com.misoft.cafe.entity.Category;
 import com.misoft.cafe.service.CategoryService;
 import com.misoft.cafe.utils.CafeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -21,6 +24,26 @@ public class CategoryControllerImpl implements CategoryController {
     public ResponseEntity<String> addNewCategory(Map<String, String> requestMap) {
         try {
             return categoryService.addNewCategory(requestMap);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<List<Category>> getAllCategory(String filterValue) {
+        try {
+            return categoryService.getAllCategory(filterValue);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<String> updateCategory(Map<String, String> requestMap) {
+        try {
+            return categoryService.updateCategory(requestMap);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
