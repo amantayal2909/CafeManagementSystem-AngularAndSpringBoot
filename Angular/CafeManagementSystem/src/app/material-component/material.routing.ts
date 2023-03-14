e33af813de3cbe7a09037c65f75756a3c2c20475
Mazharul Islam Rakib <mirakib33@gmail.com>
2023-03-14 23:44:10 +0600

    user page completed

--- Angular/CafeManagementSystem/src/app/material-component/material.routing.ts
+++ Angular/CafeManagementSystem/src/app/material-component/material.routing.ts
@@ -4,6 +4,7 @@ import { RouteGuardService } from '../services/route-guard.service';
 import { ManageCategoryComponent } from './manage-category/manage-category.component';
 import { ManageOrderComponent } from './manage-order/manage-order.component';
 import { ManageProductComponent } from './manage-product/manage-product.component';
+import { ManageUserComponent } from './manage-user/manage-user.component';
 import { ViewBillComponent } from './view-bill/view-bill.component';
 
 
@@ -27,6 +28,11 @@ export const MaterialRoutes: Routes = [
     {
         path: 'bill', component: ViewBillComponent,
         canActivate: [RouteGuardService], data: { expectedRole: ['admin', 'user'] }
+    },
+
+    {
+        path: 'user', component: ManageUserComponent,
+        canActivate: [RouteGuardService], data: { expectedRole: ['admin'] }
     }
 
 ];
