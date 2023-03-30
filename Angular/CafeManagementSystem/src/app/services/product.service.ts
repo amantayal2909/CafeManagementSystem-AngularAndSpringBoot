60bf796b7892bc0132fff805d36b093fa3b88e02
Mazharul Islam Rakib <mirakib33@gmail.com>
2023-03-30 09:57:24 +0600

    path updated

--- Angular/CafeManagementSystem/src/app/services/product.service.ts
+++ Angular/CafeManagementSystem/src/app/services/product.service.ts
@@ -12,39 +12,39 @@ export class ProductService {
   constructor(private httpClient: HttpClient) { }
 
   add(data:any) {
-    return this.httpClient.post(this.url + "/product/add/", data, {
+    return this.httpClient.post(this.url + "/product/add", data, {
       headers: new HttpHeaders().set('Content-Type', "application/json")
     })
   }
 
   update(data:any) {
-    return this.httpClient.post(this.url + "/product/update/", data, {
+    return this.httpClient.post(this.url + "/product/update", data, {
       headers: new HttpHeaders().set('Content-Type', "application/json")
     })
   }
 
   getProducts() {
-    return this.httpClient.get(this.url + "/product/get/");
+    return this.httpClient.get(this.url + "/product/get");
   }
 
   updateStatus(data:any) {
-    return this.httpClient.post(this.url + "/product/updateStatus/", data, {
+    return this.httpClient.post(this.url + "/product/updateStatus", data, {
       headers: new HttpHeaders().set('Content-Type', "application/json")
     })
   }
 
   delete(id:any) {
-    return this.httpClient.post(this.url + "/product/delete/", + id, {
+    return this.httpClient.post(this.url + "/product/delete", + id, {
       headers: new HttpHeaders().set('Content-Type', "application/json")
     })
   }
 
   getProductsByCategory(id: any) {
-    return this.httpClient.get(this.url + "/product/getByCategory/" + id);
+    return this.httpClient.get(this.url + "/product/getByCategory" + id);
   }
 
   getById(id: any) {
-    return this.httpClient.get(this.url + "/product/getById/" + id);
+    return this.httpClient.get(this.url + "/product/getById" + id);
   }
 
 }
